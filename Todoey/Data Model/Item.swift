feccf240f7d41c1efe3299df39f2cd78e51e9154
Andre D'Souza <andredsouza@Andres-Mac.local>
2017-12-28 21:27:42 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andre D'Souza on 2017-12-28.
+//  Copyright © 2017 Andre D'Souza. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
