c5c5eb234738f4dc87f5da086571d67acce34c9b
Andre D'Souza <andredsouza@Andres-Mac.local>
2017-12-30 16:53:08 -0500

    Add SelectedCategory actions (saving, and loading), connect the item list to the parentCategory, Add optional variables, (didSet), introduced Predicates

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -36,6 +36,22 @@ class CategoryViewController: UITableViewController {
         return cell
     }
     
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
+    }
+    
+    
     //MARK - Data Manipulation Methods
     func saveCategories() {
         do {
@@ -81,11 +97,6 @@ class CategoryViewController: UITableViewController {
         }
         present(alert, animated: true, completion: nil)
     }
-    
-    
-    
-    //MARK - TableView Delegate Methods
-    
     
     
 }
